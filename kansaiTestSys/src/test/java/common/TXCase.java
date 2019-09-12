7f8a4c1cef1ccba936598213f8f6d83a4c8c9e29
adstesting2020 <ads_dl@sina.com>
2019-09-12 16:47:29 +0800

    commit by sou

--- kansaiTestSys/src/test/java/common/TXCase.java
+++ kansaiTestSys/src/test/java/common/TXCase.java
@@ -7,6 +7,11 @@ import dto.DataBean;
 public class TXCase {	
 	public static ArrayList<DataBean> DATALIST = new ArrayList<DataBean>();
 
+	public TXCase() {
+		System.out.println("list clear");
+		DATALIST.clear();
+	}
+	
 	public static void setDataInfo(String msg, String img) {
 		DataBean d1 = new DataBean();
 		d1.setMsg(msg);
