4fab8a6826bdf5af63c508f83b7d62bce8c70116
adstesting2020 <ondor@192.168.200.108>
2019-09-16 17:33:40 +0800

    commit by Qin add cases

--- kansaiTestSys/src/test/java/model/Task07Cmd.java
+++ kansaiTestSys/src/test/java/model/Task07Cmd.java
@@ -15,17 +15,46 @@ public class Task07Cmd {
 		this.kb = _kb;
 	}
 
-	public void testTask07() throws Exception{
-		// S01-1-1
+	public void testTask07() throws Exception {
+		// S01-1-1 QsQ
 		kb.setTestCaseId("S01-1-1");
 		new CaseS01_001().action(driver, kb);
 
-		// S01-1-2
+		// S01-1-2 QsQ
 		kb.setTestCaseId("S01-1-2");
 		new CaseS01_002().action(driver, kb);
 
-		// S01-1-3
-		kb.setTestCaseId("S01-2-1");
+		// S01-1-3 QsQ
+		kb.setTestCaseId("S01-1-3");
 		new CaseS01_004().action(driver, kb);
+
+		// S01-1-4 QsQ
+		kb.setTestCaseId("S01-1-4");
+		new CaseS01_004().action(driver, kb);
+
+		// S01-1-5 QsQ
+		kb.setTestCaseId("S01-1-5");
+		new CaseS01_004().action(driver, kb);
+
+		// S01-1-6 QsQ
+		kb.setTestCaseId("S01-1-6");
+		new CaseS01_001().action(driver, kb);
+
+		// S01-1-7 QsQ
+		kb.setTestCaseId("S01-1-7");
+		new CaseS01_002().action(driver, kb);
+
+		// S01-1-8 QsQ
+		kb.setTestCaseId("S01-1-8");
+		new CaseS01_004().action(driver, kb);
+
+		// S01-1-9 QsQ
+		kb.setTestCaseId("S01-1-9");
+		new CaseS01_004().action(driver, kb);
+
+		// S01-1-10 QsQ
+		kb.setTestCaseId("S01-1-10");
+		new CaseS01_004().action(driver, kb);
+
 	}
 }
