3807ff45d5e431d61bf9a7493fe8eaa9ec4298e0
adstesting2020 <ads_dl@sina.com>
2019-09-16 16:51:27 +0800

    Commit by cxb 0916

---
+++ kansaiTestSys/src/test/java/model/Task07Cmd.java
@@ -0,0 +1,31 @@
+package model;
+
+import cases.task07.mobile.s01.CaseS01_001;
+import cases.task07.mobile.s01.CaseS01_002;
+import cases.task07.mobile.s01.CaseS01_004;
+import dto.KindBean;
+import io.appium.java_client.AppiumDriver;
+
+public class Task07Cmd {
+	private AppiumDriver driver;
+	private KindBean kb;
+
+	public Task07Cmd(AppiumDriver _driver, KindBean _kb) {
+		this.driver = _driver;
+		this.kb = _kb;
+	}
+
+	public void testTask07() throws Exception{
+		// S01-1-1
+		kb.setTestCaseId("S01-1-1");
+		new CaseS01_001().action(driver, kb);
+
+		// S01-1-2
+		kb.setTestCaseId("S01-1-2");
+		new CaseS01_002().action(driver, kb);
+
+		// S01-1-3
+		kb.setTestCaseId("S01-2-1");
+		new CaseS01_004().action(driver, kb);
+	}
+}
