4efae78f254f55f70814848551cf83de07b1006c
adstesting2020 <ads_dl@sina.com>
2019-09-20 11:47:36 +0800

    commit by ljw

--- kansaiTestSys/src/test/java/project/sw/cases/task07/mobile/s02/CaseS02_001.java
+++ kansaiTestSys/src/test/java/project/sw/cases/task07/mobile/s02/CaseS02_001.java
@@ -15,22 +15,22 @@ public class CaseS02_001 extends BaseCase {
 		saveReportInfo("即契約確認・エリア判定・申込みメニュー画面", "mainHeadClick.jpg");
 		
 		hpObj.btn1_2Click("btn1_2Click.jpg");
-		saveReportInfo("", "btn1_2Click.jpg");
+		saveReportInfo("即契約確認・エリア判定・申込みメニュー画面", "btn1_2Click.jpg");
 		
 		hpObj.btn2_1Click("btn2_1Click.jpg");
-		saveReportInfo("", "btn2_1Click.jpg");
+		saveReportInfo("即契約確認・エリア判定・申込みメニュー画面", "btn2_1Click.jpg");
 		
 		hpObj.btn3_1Click("btn3_1Click.jpg");
-		saveReportInfo("", "btn3_1Click.jpg");
+		saveReportInfo("即契約確認・エリア判定・申込みメニュー画面", "btn3_1Click.jpg");
 		
 		hpObj.setInput1("input1.jpg", "01");
 		hpObj.setInput2("input2.jpg", "14");
 		hpObj.setInput3("input3.jpg", "5122");
 		hpObj.setInput4("input4.jpg", "130533");
-		saveReportInfo("", "input4.jpg");
+		saveReportInfo("即契約確認・エリア判定・申込みメニュー画面", "input4.jpg");
 		
 		hpObj.clickNITTEI("Nittei.jpg");
-		saveReportInfo("", "Nittei.jpg");
+		saveReportInfo("即契約確認・エリア判定・申込みメニュー画面", "Nittei.jpg");
 		// 
 		
 		// 输出报告
