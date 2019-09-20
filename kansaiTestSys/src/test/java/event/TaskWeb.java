e8f7c31094c65f966464bac8630f5b8418eef893
adstesting2020 <ondor@192.168.200.107>
2019-09-20 14:31:43 +0800

    commit by qin

--- kansaiTestSys/src/test/java/event/TaskWeb.java
+++ kansaiTestSys/src/test/java/event/TaskWeb.java
@@ -21,7 +21,7 @@ public class TaskWeb extends BaseCase {
 	private void test03() throws InterruptedException, Exception {
 		open("https://keiyaku.kepco.jp/switch/switch_01");
 		Thread.sleep(2000);
-		kb.setTestCaseId("S03-05&06&08-10");
+		kb.setTestCaseId("S03-06&08-10");
 		new CaseS03_003().action(kb);
 		close();
 	}
@@ -30,7 +30,7 @@ public class TaskWeb extends BaseCase {
 		open("https://keiyaku.kepco.jp/switch/switch_01");
 		Thread.sleep(2000);
 		// S03-02&03&07 QsQ
-		kb.setTestCaseId("S03-02&03&07");
+		kb.setTestCaseId("S03-02&03&05&07");
 		new CaseS03_002().action(kb);
 		close();
 	}
